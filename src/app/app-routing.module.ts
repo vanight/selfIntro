00dff8c6c04e928da04e3d9385ae9f164d430863
VANight <chaoyu.bupter@gmail.com>
2019-02-12 06:14:55 +0000

    add head and career component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CareerComponent } from './career/career.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: CareerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CareerComponent } from './career/career.component';
+import { MaterialModule } from './material/material.module';
+import { HeaderComponent } from './header/header.component'
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CareerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
